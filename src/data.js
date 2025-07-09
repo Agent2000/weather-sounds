2b7bb48b0af5431ff4af86d8b09d015c81dfca3d
asn <nas_box@rambler.ru>
2025-07-09 14:22:07 +0300

    feat(index.html | *.js): add play, pause , volume

--- src/data.js
+++ src/data.js
@@ -1,5 +1,16 @@
-const textRain = "Звук дождя";
+const TextSound = {
+  rain: "Дождь",
+  winter: "Зима",
+  summer: "Лето",
+};
+
+const Sounds = {
+  RAIN: "rain",
+  WINTER: "winter",
+  SUMMER: "summer",
+};
 
 module.exports = {
-  textRain,
+  TextSound,
+  Sounds,
 };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,53 @@
-import { textRain } from "./data";
+import { TextSound, Sounds } from "./data";
 
 const root = document.getElementById("main");
 const h2 = document.createElement("h2");
-const audioRain = document.getElementById("audio-rain");
+const rain = document.getElementById("audio-rain");
+const summer = document.getElementById("audio-summer");
+const winter = document.getElementById("audio-winter");
 const playBtnRain = document.getElementById("play-rain");
+const playBtnWinter = document.getElementById("play-winter");
+const playBtnSummer = document.getElementById("play-summer");
+const volume = document.getElementById("volume");
 
-h2.innerText = "Привет javascript!";
-root.append(h2);
-playBtnRain.textContent = textRain;
+let currenSound = {};
+
+playBtnRain.textContent = TextSound.rain;
+playBtnWinter.textContent = TextSound.winter;
+playBtnSummer.textContent = TextSound.summer;
+
+const onItemClick = function (item, sound) {
+  // остановить текущий звук
+  if (currenSound.id && currenSound.id !== sound) {
+    currenSound.sound.pause();
+  }
+
+  item.volume = volume.value;
+
+  currenSound.id = sound;
+  currenSound.sound = item;
+
+  if (item.paused) {
+    item.play();
+  } else {
+    item.pause();
+  }
+};
 
 playBtnRain.addEventListener("click", function () {
-  if (audioRain.paused) {
-    audioRain.play();
-    playBtnRain.textContent = textRain + " ||";
-  } else {
-    audioRain.pause();
-    playBtnRain.textContent = textRain + " >";
+  onItemClick(rain, Sounds.RAIN);
+});
+
+playBtnSummer.addEventListener("click", function () {
+  onItemClick(summer, Sounds.SUMMER);
+});
+
+playBtnWinter.addEventListener("click", function () {
+  onItemClick(winter, Sounds.WINTER);
+});
+
+volume.addEventListener("input", function () {
+  if (currenSound.id) {
+    currenSound.sound.volume = this.value;
   }
 });
