a2377e0e226402db42e5749aba57244a87a04ecb
asn <nas_box@rambler.ru>
2025-07-20 18:55:38 +0300

    config: add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
+};
