8d7053c751b3e92e8abf17e1f855d29b832d6726
asn <nas_box@rambler.ru>
2025-07-30 13:25:14 +0300

    feat(data.ts): add file

---
+++ src/data.ts
@@ -0,0 +1,31 @@
+type Season = { [key: string]: string };
+
+export const TextSound: Season = {
+  rain: "Дождь",
+  winter: "Зима",
+  summer: "Лето",
+};
+
+export const Sounds: Season = {
+  rain: "rain",
+  winter: "winter",
+  summer: "summer",
+};
+
+export const PathBg: Season = {
+  rain: "assets/rainy-bg.jpg",
+  winter: "assets/winter-bg.jpg",
+  summer: "assets/summer-bg.jpg",
+};
+
+export const PathIcons: Season = {
+  rain: "assets/icons/cloud-rain.svg",
+  winter: "assets/icons/cloud-snow.svg",
+  summer: "assets/icons/sun.svg",
+};
+
+export const BtnIds: Season = {
+  rain: "btnRain",
+  winter: "btnWinter",
+  summer: "btnSummer",
+};
